842a231af83698477689c7f0a39e6539fd69b86f
66ljq <962375193@qq.com>
2018-12-26 00:54:15 +0800

    留言本

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import CommentList from './components/CommentList'
+import CommentBox from './components/CommentBox'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      comments: ['this is my first reply']
+    }
+    this.addComment = this.addComment.bind(this)
+  }
+  addComment(comment) {
+    this.setState({
+      comments: [...this.state.comments, comment]
+    })
+  }
   render() {
+    const { comments } = this.state
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </header> */}
+        <CommentList comments={comments}/>
+        <CommentBox commentsLength={comments.length}
+                    onAddComment={this.addComment}/>
       </div>
     );
   }
